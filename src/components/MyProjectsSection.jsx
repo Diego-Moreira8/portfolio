e965c7cbe0015b365dbe7ae5bbc749c22b967999
Diego Moreira de Oliveira <diego.m.oliveira@una.br>
2023-09-14 13:27:43 -0300

    feat: add Project component

--- src/components/MyProjectsSection.jsx
+++ src/components/MyProjectsSection.jsx
@@ -1,11 +1,22 @@
-import { styled } from "styled-components";
-
 import { Heading1 } from "./Headings";
+import Project from "./Project";
+
+import screenshotTest from "../images/ss.png";
 
 export default function MyProjectsSection() {
   return (
     <section>
       <Heading1 text={"Meus Projetos"} />
+      <Project
+        title={"Nome do Projeto"}
+        src={screenshotTest}
+        alt={"Texto alternativo de teste"}
+        description={
+          "Descrição do projeto aqui. Asd ef wef we wie weir naioudhasd asd asudasdo. "
+        }
+        repoUrl={"#"}
+        projectUrl={"#"}
+      />
     </section>
   );
 }
---
+++ src/components/Project.jsx
@@ -0,0 +1,66 @@
+import { styled } from "styled-components";
+
+const StyledContainer = styled.div`
+  margin: 1rem;
+  border: 2px solid ${(props) => props.theme.text};
+
+  h2 {
+    padding: 0.5rem;
+    background-color: ${(props) => props.theme.text};
+    color: ${(props) => props.theme.background};
+    font-weight: bold;
+    font-size: 1.5rem;
+    text-align: center;
+  }
+
+  img {
+    aspect-ratio: 16 / 9;
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+    object-position: top;
+    border-bottom: 2px solid ${(props) => props.theme.text};
+  }
+
+  > div {
+    padding: 1rem;
+
+    p {
+      margin-bottom: 1rem;
+    }
+
+    .links {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      gap: 1rem;
+
+      a {
+        text-align: center;
+        display: block;
+      }
+    }
+  }
+`;
+
+export default function Project({
+  title,
+  src,
+  alt,
+  description,
+  repoUrl,
+  projectUrl,
+}) {
+  return (
+    <StyledContainer>
+      <h2>{title}</h2>
+      <img src={src} alt={alt} title={alt} />
+      <div>
+        <p>{description}</p>
+        <div className="links">
+          <a href={repoUrl}>Repositório no GitHub</a>
+          <a href={projectUrl}>Site do projeto</a>
+        </div>
+      </div>
+    </StyledContainer>
+  );
+}
