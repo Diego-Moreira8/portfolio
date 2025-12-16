d12c1c7459a1900a5795e1cc45538d5d33700093
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2025-12-16 19:56:16 -0300

    feat: links de contato

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,6 +4,7 @@ import { pageSections } from "../utils/page-sections";
 import { Heading1 } from "./Headings";
 import { LinkButton } from "./LinkButton";
 import { SectionContainer } from "./SectionContainer";
+import { EMAIL_URL, LINKEDIN_URL, WHATSAPP_URL } from "../utils/strings";
 
 export function Contact() {
   const { id, title, url } = pageSections.contact;
@@ -13,14 +14,22 @@ export function Contact() {
       <Heading1 url={url}>{title}</Heading1>
 
       <LinkButton
-        url={"#"}
+        url={WHATSAPP_URL}
         Icon={FaWhatsapp}
         text={"Envie uma mensagem no WhatsApp"}
       />
 
-      <LinkButton url={"#"} Icon={FaLinkedin} text={"Conecte-se no LinkedIn"} />
+      <LinkButton
+        url={LINKEDIN_URL}
+        Icon={FaLinkedin}
+        text={"Conecte-se no LinkedIn"}
+      />
 
-      <LinkButton url={"#"} Icon={MdOutlineEmail} text={"Envie um e-mail"} />
+      <LinkButton
+        url={EMAIL_URL}
+        Icon={MdOutlineEmail}
+        text={"Envie um e-mail"}
+      />
     </SectionContainer>
   );
 }
--- src/utils/strings.js
+++ src/utils/strings.js
@@ -2,3 +2,9 @@ export const CV_URL =
   "https://drive.google.com/file/d/10QXaG1fJagvM0khBkZPw385jUb245Iy6/view?usp=sharing";
 
 export const GITHUB_URL = "https://github.com/Diego-Moreira8";
+
+export const WHATSAPP_URL = "https://wa.me/5564993380234";
+
+export const LINKEDIN_URL = "https://www.linkedin.com/in/diego-moreira8";
+
+export const EMAIL_URL = "mailto:diego.moreira529@gmail.com";
