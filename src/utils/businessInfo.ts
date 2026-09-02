0b45e9a83ddc22ccdd36fc6729ed9cf8f3038f7e
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2026-09-02 15:56:34 -0300

    feat: component Head

---
+++ src/utils/businessInfo.ts
@@ -0,0 +1,10 @@
+export const businessInfo = {
+  name: "Diego Web Dev",
+
+  description:
+    "Desenvolvedor web focado em Astro, React e Node.JS. " +
+    "Crio sites e apps modernos, acessíveis e performáticos, " +
+    "com código limpo e foco na experiência do usuário.",
+
+  site: "https://diegowebdev.com.br/",
+};
