cae6884eecdc7b275df6458eae72a56113303707
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2026-09-02 10:06:52 -0300

    feat: Cloudflare configurado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://diegowebdev.com.br/",
+
+  output: "static",
+
+  server: {
+    host: true,
+  },
+
+  devToolbar: {
+    enabled: false,
+  },
+
+  // https://docs.astro.build/en/guides/integrations-guide/cloudflare/
+  adapter: cloudflare({ imageService: "custom" }),
+});
