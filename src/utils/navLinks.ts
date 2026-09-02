835262abf37d4b3a561c646c756fbfc449c9a7ba
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2026-09-02 19:09:37 -0300

    feat: nav para mobile

---
+++ src/utils/navLinks.ts
@@ -0,0 +1,6 @@
+export const navLinks = [
+  { label: "Home", href: "#home" },
+  { label: "Projetos", href: "#projetos" },
+  { label: "Serviços", href: "#servicos" },
+  { label: "Sobre Mim", href: "#sobre-mim" },
+];
